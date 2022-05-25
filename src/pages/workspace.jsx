f9cedbd87bf831485bb76d07cf37b2b92a501eb4
System Administrator <eranavichzer@gmail.com>
2022-05-25 14:06:43 +0300

    finish step 1 import all board plus basic scss

--- src/pages/workspace.jsx
+++ src/pages/workspace.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { loadBoards } from '../store/actions/board.action.js'
 
+import { loadBoards } from '../store/actions/board.action.js'
 import { boardService } from '../services/board.service.js'
 
+import { BoardList } from '../cmps/board-list.jsx'
 
 const _Workspace = ({ loadBoards, boards }) => {
 
@@ -11,21 +12,20 @@ const _Workspace = ({ loadBoards, boards }) => {
       loadBoards()
    }, [])
 
-   return <main>Workspace
-      {boards[0]._id}
+   return (<main>
 
-   </main>
+      <BoardList boards={boards} />
+   </main> )
 
 }
 
 function mapStateToProps(state) {
    return {
-       boards: state.boardModule.boards
+      boards: state.boardModule.boards
    }
 }
 const mapDispatchToProps = {
    loadBoards
 }
-
 
 export const Workspace = connect(mapStateToProps, mapDispatchToProps)(_Workspace)
