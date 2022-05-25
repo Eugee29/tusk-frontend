ac568da2cb6eae75fc82c6e4b8c88c50d0393fd3
System Administrator <eranavichzer@gmail.com>
2022-05-25 13:06:49 +0300

    first commit for workspace page - imports and get all boards

--- src/pages/workspace.jsx
+++ src/pages/workspace.jsx
@@ -1,3 +1,31 @@
-export const Workspace = () => {
-  return <main>Workspace</main>
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { loadBoards } from '../store/actions/board.action.js'
+
+import { boardService } from '../services/board.service.js'
+
+
+const _Workspace = ({ loadBoards, boards }) => {
+
+   useEffect(() => {
+      loadBoards()
+   }, [])
+
+   return <main>Workspace
+      {boards[0]._id}
+
+   </main>
+
 }
+
+function mapStateToProps(state) {
+   return {
+       boards: state.boardModule.boards
+   }
+}
+const mapDispatchToProps = {
+   loadBoards
+}
+
+
+export const Workspace = connect(mapStateToProps, mapDispatchToProps)(_Workspace)
