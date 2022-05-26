20d46189f1a8511d4f2424787befef9a4bb94df2
Uri Gruda <uri2920@gmail.com>
2022-05-26 12:14:26 +0300

    added drag and drop to groups

--- src/cmps/group-preview.jsx
+++ src/cmps/group-preview.jsx
@@ -1,22 +1,31 @@
 import React from "react"
 import { BsThreeDots } from 'react-icons/bs'
+import { Draggable } from 'react-beautiful-dnd'
 
 import { TaskList } from './task-list'
 
-export function GroupPreview({ group }) {
+export function GroupPreview({ group, index }) {
 
   return (
-    <section className="group-preview">
+    <Draggable draggableId={group.id} index={index} >
+      {(provided) => (
+        <section className="group-preview"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
 
-      <div className="group-title-container">
-        <textarea className="group-title" name="" defaultValue={group.title} cols="30" rows="10"></textarea>
-        <button> <BsThreeDots /> </button>
-      </div>
+          <div className="group-title-container">
+            <textarea className="group-title" name="" defaultValue={group.title} cols="30" rows="10"></textarea>
+            <button> <BsThreeDots /> </button>
+          </div>
 
-      <section className="tasks-container">
-        <TaskList key={group.id} tasks={group.tasks} />
-      </section>
+          <section className="tasks-container">
+            <TaskList key={group.id} tasks={group.tasks} />
+          </section>
 
-    </section>
+        </section>
+      )}
+    </Draggable>
   )
 }
--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -8,5 +8,4 @@ export function TaskPreview({ task }) {
       <button> <RiPencilLine className='btn-icon' /> </button>
     </section>
   )
-
 }
