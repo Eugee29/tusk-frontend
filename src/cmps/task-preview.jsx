befe86690643b75b8b5cc882419aaff02230e933
TalAOfer <talofer123@gmail.com>
2022-05-27 13:22:58 +0300

    added labels, desc icon, etc. to boardPreview

--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -1,13 +1,19 @@
 
 import { Draggable } from 'react-beautiful-dnd'
-import { RiPencilLine } from 'react-icons/ri'
+
 import { useNavigate } from 'react-router-dom'
+
+import { LabelList } from './label-list'
+
+import { RiPencilLine } from 'react-icons/ri'
 import { MdOutlineSubject } from 'react-icons/md'
+import { IoMdCheckboxOutline } from 'react-icons/io'
 
 import styled from "styled-components"
 
 export function TaskPreview({ task, groupId, index }) {
   const navigate = useNavigate()
+  
   const onOpenDetails = (ev) => {
     ev.stopPropagation()
     navigate(`${groupId}/${task.id}`)
@@ -45,10 +51,11 @@ export function TaskPreview({ task, groupId, index }) {
     <Draggable draggableId={task.id} index={index} type='TASK' >
       {provided => (
         <Container className={getTaskClass()} onClick={onOpenDetails} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}  >
+          {task.labelIds.length && (!task.style.bgColor) && (!task.style.imgURL) && <LabelList labelIds={task.labelIds} />}
           <div className='task-title-container'>
             <h2 className='task-title'> {task.title} </h2>
             {task.description && (!task.style.bgColor) && (!task.style.imgURL)
-              && <MdOutlineSubject />}
+              && <MdOutlineSubject className='lines-icon' />}
           </div>
           <button className='edit-btn'> <RiPencilLine className='btn-icon' /> </button>
         </Container>
