e36fac79244cfe92a877441fc19db044237b2f4a
TalAOfer <talofer123@gmail.com>
2022-05-30 18:19:03 +0300

    adding groupprev modal

--- src/cmps/modal.jsx
+++ src/cmps/modal.jsx
@@ -12,29 +12,34 @@ import { CgClose } from 'react-icons/cg'
 import { IoIosArrowBack } from 'react-icons/io'
 import { useDispatch } from 'react-redux'
 import { setModal } from '../store/app/app.actions'
+import { ModalGroupActions } from './modal-group-actions'
 
 export const Modal = ({ task, category, pos, board, onUpdateBoard, updateTask, onOpenModalDynamic }) => {
 
   const { modal } = useSelector(({ appModule }) => appModule)
   const dispatch = useDispatch()
   const editLabel = useRef('')
+  var cmp
 
   const changeEditLabel = (label) => {
     editLabel.current = label
     onOpenModalDynamic('Change label')
   }
+  
+  if (!modal) return
 
-  switch (category) {
+  switch (modal.category) {
     case 'Cover':
       pos.top += 50
       break
-
+    case 'Group actions': 
+      modal.position.top += 28
+      cmp = <ModalGroupActions groupId={modal.groupId}/>
     default:
       break
   }
 
 
-  if (!modal) return
   // modal.position.top += 30
 
   return (
@@ -47,12 +52,13 @@ export const Modal = ({ task, category, pos, board, onUpdateBoard, updateTask, o
       </header>
 
       <main className="main-modal">
-
-        {category === 'Cover' && <ModalCover task={task} />}
+        {cmp}
+        {/* {category === 'Cover' && <ModalCover task={task} />}
         {category === 'Members' && <ModalMember task={task} updateTask={updateTask} board={board} />}
         {category === 'Labels' && <ModalLabel task={task} updateTask={updateTask} board={board} onOpenModalDynamic={onOpenModalDynamic} changeEditLabel={changeEditLabel} />}
         {category === 'Create label' && <ModalLabelCreate task={task} board={board} onUpdateBoard={onUpdateBoard} onOpenModalDynamic={onOpenModalDynamic} />}
         {category === 'Change label' && <ModalLabelChange task={task} board={board} editLabel={editLabel.current} onOpenModalDynamic={onOpenModalDynamic} updateTask={updateTask} onUpdateBoard={onUpdateBoard} />}
+        {category === 'Group actions' && cmp} */}
       </main>
 
     </div>)
