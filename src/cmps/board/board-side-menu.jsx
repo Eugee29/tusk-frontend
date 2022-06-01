b87500d250cc5ecf14e3d0b262dd76665a76c5a9
Uri Gruda <uri2920@gmail.com>
2022-06-01 18:50:06 +0300

    merge

--- src/cmps/board/board-side-menu.jsx
+++ src/cmps/board/board-side-menu.jsx
@@ -10,7 +10,7 @@ export function BoardSideMenu({ onToggleMenu, dynamicClass, board }) {
     <section className={dynamicClass}>
       <button className='x-btn' onClick={onToggleMenu}> <IoMdClose className='x-icon' /> </button>
       <div className='menu-title'> <h3> Menu </h3>  </div>
-      {board.activities.length > 0 && <ActivityList board={board} onToggleMenu={onToggleMenu}/>}
+      {board.activities.length > 0 && <ActivityList board={board} onToggleMenu={onToggleMenu} />}
     </section>
   )
 }
