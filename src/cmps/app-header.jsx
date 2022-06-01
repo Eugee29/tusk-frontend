6a22ba1be9def08dd4f4dc66c4063320a743447e
Uri Gruda <uri2920@gmail.com>
2022-06-01 13:33:37 +0300

    merge

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,6 +1,5 @@
 import { useLocation, Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { onLogout } from '../store/user/user.action.js'
 
@@ -9,59 +8,59 @@ import logoDark from '../assets/imgs/logo-horizontal-dark.png'
 
 export const AppHeader = () => {
 
-   const { user } = useSelector(({ userModule }) => userModule)
-   const { pathname } = useLocation()
-   const dispatch = useDispatch()
+  const { user } = useSelector(({ userModule }) => userModule)
+  const { pathname } = useLocation()
+  const dispatch = useDispatch()
 
-   if (pathname === '/login' || pathname === '/signup') return
+  if (pathname === '/login' || pathname === '/signup') return
 
-   const isHome = pathname === '/'
-   const isBoard = (pathname.includes('/board'))
-   const initials = (user) => ([...user.fullname])
+  const isHome = pathname === '/'
+  const isBoard = (pathname.includes('/board'))
+  const initials = (user) => ([...user.fullname])
 
-   const getClassname = () => {
-      if (isHome) return 'home'
-      if (isBoard) return 'board'
-      else return 'general'
-   }
+  const getClassName = () => {
+    if (isHome) return 'home'
+    if (isBoard) return 'board'
+    else return 'general'
+  }
 
-   const logout = async () => {
-      dispatch(onLogout())
-   }
+  const logout = async () => {
+    dispatch(onLogout())
+  }
 
-   return (
-      <header className={`app-header ${getClassname()}`} >
-         <nav className='link-container'>
-            <div className='logo-container'>
-               <Link to='/'>
-                  {
-                     isHome ?
-                        <img src={logoDark} alt="tusk-logo" className='logo' />
-                        :
-                        <img src={logoLight} alt="tusk-logo" className='logo' />
-                  }
-               </Link>
-            </div>
-            {!isHome && <Link className='workspace-link' to='/workspace'>Workspace</Link>}
-         </nav>
-         {
-            isHome &&
-            <nav className='login-signup-container'>
-               <Link to='/login' className='login'>Log in</Link>
-               <Link to='/signup' className='signup'>Sign up</Link>
-            </nav>
-         }
-         {
-            !isHome &&
-            <div className='user-img-container' onClick={logout}>
-               {user &&
-                  (user?.imgURL
-                  ? <span className="user-img" style={{ backgroundImage: `url('${user.imgURL}')` }}></span>
-                  : <span className="user-initial" >{`${initials(user)[0]}${initials(user)[1]}`}</span>)
-               }
-               {!user && <span className="user-initial" ></span>}
-            </div>
-         }
-      </header >
-   )
+  return (
+    <header className={`app-header ${getClassName()}`} >
+      <nav className='link-container'>
+        <div className='logo-container'>
+          <Link to='/'>
+            {
+              isHome ?
+                <img src={logoDark} alt="tusk-logo" className='logo' />
+                :
+                <img src={logoLight} alt="tusk-logo" className='logo' />
+            }
+          </Link>
+        </div>
+        {!isHome && <Link className='workspace-link' to='/workspace'>Workspace</Link>}
+      </nav>
+      {
+        isHome &&
+        <nav className='login-signup-container'>
+          <Link to='/login' className='login'>Log in</Link>
+          <Link to='/signup' className='signup'>Sign up</Link>
+        </nav>
+      }
+      {
+        !isHome &&
+        <div className='user-img-container' onClick={logout}>
+          {user &&
+            (user?.imgURL
+              ? <span className="user-img" style={{ backgroundImage: `url('${user.imgURL}')` }}></span>
+              : <span className="user-initial" >{`${initials(user)[0]}${initials(user)[1]}`}</span>)
+          }
+          {!user && <span className="user-initial" ></span>}
+        </div>
+      }
+    </header >
+  )
 }
