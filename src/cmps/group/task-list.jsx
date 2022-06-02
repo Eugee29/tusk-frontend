5a274ec4800422f0a88d575498a8021b2e5077f4
Uri Gruda <uri2920@gmail.com>
2022-06-02 21:02:01 +0300

    some changes

--- src/cmps/group/task-list.jsx
+++ src/cmps/group/task-list.jsx
@@ -9,16 +9,17 @@ import { boardService } from '../../services/board.service'
 export const TaskList = ({ group, board, isLabelsOpen, toggleLabels, isAddCardOpen, toggleAddCard, onUpdateGroup, onUpdateBoard }) => {
   const [cardText, setCardText] = useState('')
 
-  const handleChange = (ev) => {
-    setCardText(ev.target.value)
+  const handleChange = ({ target, nativeEvent }) => {
+    if (nativeEvent.inputType === 'insertLineBreak') return onAddCard()
+    setCardText(target.value)
   }
 
   const onAddCard = () => {
     toggleAddCard()
     if (!cardText) return
-    // ?
     const taskToAdd = boardService.getEmptyTask(cardText)
     addCard(taskToAdd)
+    setCardText('')
   }
 
   const addCard = (taskToAdd) => {
@@ -46,7 +47,7 @@ export const TaskList = ({ group, board, isLabelsOpen, toggleLabels, isAddCardOp
           {group.tasks.map((task, index) => !task.archivedAt && <TaskPreview key={task.id} board={board} groupId={group.id} task={task} index={index} toggleLabels={toggleLabels} isLabelsOpen={isLabelsOpen} onUpdateBoard={onUpdateBoard} />)}
           {provided.placeholder}
           {isAddCardOpen && <div className="add-card-container">
-            <textarea autoFocus onBlur={onAddCard} placeholder='Enter a title for this card...' onChange={handleChange}></textarea>
+            <textarea autoFocus onBlur={onAddCard} placeholder='Enter a title for this card...' onChange={handleChange} value={cardText}></textarea>
             <div className='btn-container'>
               <button className='add-btn' onClick={onAddCard}>Add card</button>
               <button className='x-btn' onClick={toggleAddCard}> <IoMdClose className='x-icon' /> </button>
