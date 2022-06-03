c37eacf06f6d577cd75e7f81bab432369fea05ca
Uri Gruda <uri2920@gmail.com>
2022-06-03 15:38:22 +0300

    some fixes

--- src/cmps/login-signup/signup-form.jsx
+++ src/cmps/login-signup/signup-form.jsx
@@ -1,38 +1,68 @@
-import React, { useState, } from 'react'
+import React, { useState } from 'react'
 
 export const SignupForm = ({ onSignup }) => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+    fullname: '',
+  })
 
-   const [credentials, setCredentials] = useState({ username: '', password: '', fullname: '' })
+  const handleChange = (ev) => {
+    const field = ev.target.namee
+    const value = ev.target.value
+    setCredentials({ ...credentials, [field]: value })
+  }
 
-   const handleChange = (ev) => {
-      const field = ev.target.name;
-      const value = ev.target.value;
-      setCredentials({ ...credentials, [field]: value });
-   }
+  const signup = (ev = null) => {
+    if (!credentials.username || !credentials.password || !credentials.fullname)
+      return
+    if (ev) {
+      ev.preventDefault()
+      ev.stopPropagation()
+    }
+    onSignup(credentials)
+    clearState()
+  }
 
-   const signup = (ev = null) => {
-      if (!credentials.username || !credentials.password || !credentials.fullname) return;
-      if (ev) ev.preventDefault();
-      if (ev) ev.stopPropagation();
-      onSignup(credentials);
-      clearState()   
-   }
+  const clearState = () => {
+    setCredentials({
+      username: '',
+      password: '',
+      fullname: '',
+    })
+  }
 
-   const clearState = () => {
-      setCredentials({
-         username: '',
-         password: '',
-         fullname: ''
-      })
-   }
-
-   return (
-      <form className='user-signup' onSubmit={signup}>
-         <h1>Sign up for your account</h1>
-         <input type="email" name='username' placeholder='Enter email' value={credentials.username} onChange={handleChange} required autoFocus />
-         <input type="text" name='fullname' placeholder='Enter full name' value={credentials.fullname} autoComplete='username' onChange={handleChange} required />
-         <input type="password" name='password' placeholder='Enter password' value={credentials.password} autoComplete='current-password' onChange={handleChange} required />
-         <button className='signup-btn'>Sign up</button>
-      </form>
-   )
+  return (
+    <form className="user-signup" onSubmit={signup}>
+      <h1>Sign up for your account</h1>
+      <input
+        type="email"
+        name="username"
+        placeholder="Enter email"
+        value={credentials.username}
+        onChange={handleChange}
+        required
+        autoFocus
+      />
+      <input
+        type="text"
+        name="fullname"
+        placeholder="Enter full name"
+        value={credentials.fullname}
+        autoComplete="username"
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter password"
+        value={credentials.password}
+        autoComplete="current-password"
+        onChange={handleChange}
+        required
+      />
+      <button className="signup-btn">Sign up</button>
+    </form>
+  )
 }
