7b6bb5bc254d95754c2692d967a7bf4edf226858
Eran Avichzer <eranavichzer@gmail.com>
2022-06-04 21:32:09 +0300

    add archived msg

--- src/cmps/archived-list.jsx
+++ src/cmps/archived-list.jsx
@@ -4,14 +4,18 @@ export function ArchivedList({ board, onUpdateBoard }) {
 
    if (!board.groups) return
 
-   const groups = board.groups.map(group => group.tasks.map(task => ({groupId: group.id, task})))
+   const groups = board.groups.map(group => group.tasks.map(task => ({ groupId: group.id, task })))
    const tasks = []
    for (let i = 0; i < groups.length; i++) { tasks.push(...groups[i]) }
 
    return (
       <div className='group-preview archived'>
          <div className="task-list archived">
-            {tasks.map((task, index) => task.task.archivedAt && <ArchivedPreview key={task.task.id} board={board} groupId={task.groupId} task={task.task} index={index} onUpdateBoard={onUpdateBoard} />)}
+            { tasks.some(task => task.task.archivedAt) 
+            ? tasks.map((task, index) => task.task.archivedAt &&
+               <ArchivedPreview key={task.task.id} board={board} groupId={task.groupId} task={task.task} index={index} onUpdateBoard={onUpdateBoard} />)
+               : <div className="empty">Archived empty</div> 
+            }     
          </div>
       </div>
    )
