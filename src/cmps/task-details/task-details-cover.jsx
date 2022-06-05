6602d6a099065e6e3219f55bed3554ac19f566ef
Eran Avichzer <eranavichzer@gmail.com>
2022-06-05 11:13:08 +0300

    preview with video at task details

--- src/cmps/task-details/task-details-cover.jsx
+++ src/cmps/task-details/task-details-cover.jsx
@@ -2,74 +2,66 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { setModal } from '../../store/app/app.actions'
-import FastAverageColor from 'fast-average-color'
+import { utilService } from '../../services/util.service'
 
+import FastAverageColor from 'fast-average-color'
 import { FaWindowMaximize } from 'react-icons/fa'
 
 export function TaskDetailsCover({ task, updateTask }) {
-  const buttonRef = useRef()
-  const [bgColor, setBgColor] = useState(null)
-  const dispatch = useDispatch()
+   const buttonRef = useRef()
+   const [bgColor, setBgColor] = useState(null)
+   const dispatch = useDispatch()
 
-  const fac = new FastAverageColor()
+   const fac = new FastAverageColor()
 
-  useEffect(() => {
-    if (task?.style?.imgURL) loadBgColor()
-  }, [task])
+   useEffect(() => {
+      if (task?.style?.imgURL) loadBgColor()
+   }, [task])
 
-  const loadBgColor = async () => {
-    const color = await fac.getColorAsync(task.style.imgURL)
-    setBgColor(color.hexa)
-  }
+   const loadBgColor = async () => {
+      const color = await fac.getColorAsync(task.style.imgURL)
+      setBgColor(color.hexa)
+   }
 
-  const onModal = (category) => {
-    dispatch(
-      setModal({
-        element: buttonRef.current,
-        category,
-        title: category,
-        props: { task, updateTask },
-      })
-    )
-  }
+   const onModal = (category) => {
+      dispatch(
+         setModal({
+            element: buttonRef.current,
+            category,
+            title: category,
+            props: { task, updateTask },
+         })
+      )
+   }
 
-  return (
-    <header>
-      {task?.style?.bgColor && (
-        <div
-          className="task-details-cover color"
-          style={{ backgroundColor: `${task.style.bgColor}` }}
-        >
-          <button
-            ref={buttonRef}
-            onClick={(ev) => {
-              ev.stopPropagation()
-              onModal('Cover')
-            }}
-          >
-            <FaWindowMaximize />
-            Cover
-          </button>
-        </div>
-      )}
+   return (
+      <header>
+         {task?.style?.bgColor && (
+            <div
+               className="task-details-cover color"
+               style={{ backgroundColor: `${task.style.bgColor}` }}
+            >
+               <button
+                  ref={buttonRef}
+                  onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}>
+                  <FaWindowMaximize />
+                  Cover
+               </button>
+            </div>
+         )}
 
-      {task?.style?.imgURL && (
-        <div
-          className="task-details-cover img "
-          style={{ backgroundColor: bgColor, backgroundImage: `url('${task.style.imgURL}')` }}
-        >
-          <button
-            ref={buttonRef}
-            onClick={(ev) => {
-              ev.stopPropagation()
-              onModal('Cover')
-            }}
-          >
-            <FaWindowMaximize />
-            Cover
-          </button>
-        </div>
-      )}
-    </header>
-  )
+         {task?.style?.imgURL && utilService.getExtension(task.style.imgURL) === 'image' &&
+            (<div className="task-details-cover img " style={{ backgroundColor: bgColor, backgroundImage: `url('${task.style.imgURL}')` }}>
+               <button ref={buttonRef} onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}><FaWindowMaximize />Cover</button>
+            </div>
+            )}
+         {task?.style?.imgURL && utilService.getExtension(task.style.imgURL) === 'video' &&
+            (<div className="task-details-cover img " >    
+               <video height="160" width= '100%' muted controls><source src={task.style.imgURL} type="video/mp4"></source></video>
+               <button ref={buttonRef} onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}><FaWindowMaximize />Cover</button>
+            </div>
+            )}
+
+      </header>
+   )
 }
