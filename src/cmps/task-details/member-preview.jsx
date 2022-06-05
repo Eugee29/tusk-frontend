7684213ed5028cc585a45606b2ba27d6f46b6de4
Eran Avichzer <eranavichzer@gmail.com>
2022-06-05 15:15:11 +0300

    fixed member icon in task preview

--- src/cmps/task-details/member-preview.jsx
+++ src/cmps/task-details/member-preview.jsx
@@ -9,7 +9,6 @@ export const MemberPreview = ({ member, task, board, onUpdateBoard, isInTaskDeta
    const memberRef = useRef()
 
    const initials = (member) => ([...member.fullname])
-   
 
    const onOpenModal = (ev, modal) => {
       ev.stopPropagation()
@@ -17,20 +16,42 @@ export const MemberPreview = ({ member, task, board, onUpdateBoard, isInTaskDeta
    }
 
    return (
-      isInTaskDetails ?
-         <Fragment>
-            {member?.imgURL
-               ?
-               <a key={member._id} className="member-img"
-                  onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })}>
-                  <img src={member.imgURL} ref={memberRef} alt={member.fullName} /> </a>
-               :
-               <a className="member" ref={memberRef}
-                  onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })} >
-                  {`${initials(member)[0]}${initials(member)[1]}`}</a>
-            }
-         </Fragment>
-         :
-         <a key={member._id} className="member-img" onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })}> <img src={member.imgURL} ref={memberRef} alt={member.fullName} /> </a>
+      <Fragment>
+         {member?.imgURL
+            ?
+            <a key={member._id} className="member-img"
+               onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })}>
+               <img src={member.imgURL} ref={memberRef} alt={member.fullName} />
+            </a>
+            :
+            <a className="member" ref={memberRef}
+               onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })} >
+               {`${initials(member)[0]}${initials(member)[1]}`}
+            </a>
+         }
+      </Fragment>
    )
 }
+
+
+// return (
+//    isInTaskDetails ?
+//       <Fragment>
+//          {member?.imgURL
+//             ?
+//             <a key={member._id} className="member-img"
+//                onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })}>
+//                <img src={member.imgURL} ref={memberRef} alt={member.fullName} /> </a>
+//             :
+//             <a className="member" ref={memberRef}
+//                onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })} >
+//                {`${initials(member)[0]}${initials(member)[1]}`}</a>
+//          }
+//       </Fragment>
+//       :
+//       <a key={member._id} className="member-img"
+//          onClick={(ev) => onOpenModal(ev, { category: 'member-actions', element: memberRef.current, props: { member, task, board, onUpdateBoard } })}>
+//          <img src={member.imgURL} ref={memberRef} alt={member.fullName} />
+//       </a>
+// )
+// }
