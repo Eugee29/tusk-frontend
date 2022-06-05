f6297fc3523a7fd5799774324a1ef513f94f1558
Eran Avichzer <eranavichzer@gmail.com>
2022-06-05 12:16:03 +0300

    add fullscreen cover

--- src/cmps/task-details/task-details-cover.jsx
+++ src/cmps/task-details/task-details-cover.jsx
@@ -6,10 +6,13 @@ import { utilService } from '../../services/util.service'
 
 import FastAverageColor from 'fast-average-color'
 import { FaWindowMaximize } from 'react-icons/fa'
+import { VscClose } from 'react-icons/vsc'
+
 
 export function TaskDetailsCover({ task, updateTask }) {
    const buttonRef = useRef()
    const [bgColor, setBgColor] = useState(null)
+   const [isFullScreen, setIsFullScreen] = useState(false)
    const dispatch = useDispatch()
 
    const fac = new FastAverageColor()
@@ -21,6 +24,14 @@ export function TaskDetailsCover({ task, updateTask }) {
    const loadBgColor = async () => {
       const color = await fac.getColorAsync(task.style.imgURL)
       setBgColor(color.hexa)
+   }
+
+   const onFullScreen = () => {
+      setIsFullScreen(!isFullScreen)
+   }
+
+   const onGoBack = () => {
+      setIsFullScreen(false)
    }
 
    const onModal = (category) => {
@@ -37,30 +48,30 @@ export function TaskDetailsCover({ task, updateTask }) {
    return (
       <header>
          {task?.style?.bgColor && (
-            <div
-               className="task-details-cover color"
-               style={{ backgroundColor: `${task.style.bgColor}` }}
-            >
-               <button
-                  ref={buttonRef}
-                  onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}>
-                  <FaWindowMaximize />
-                  Cover
-               </button>
+            <div className="task-details-cover color" style={{ backgroundColor: `${task.style.bgColor}` }}>
+               <button ref={buttonRef} onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}> <FaWindowMaximize /> Cover </button>
             </div>
          )}
 
          {task?.style?.imgURL && utilService.getExtension(task.style.imgURL) === 'image' &&
-            (<div className="task-details-cover img " style={{ backgroundColor: bgColor, backgroundImage: `url('${task.style.imgURL}')` }}>
+            (<div className="task-details-cover img point" onClick={onFullScreen} style={{ backgroundColor: bgColor, backgroundImage: `url('${task.style.imgURL}')` }}>
                <button ref={buttonRef} onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}><FaWindowMaximize />Cover</button>
             </div>
             )}
          {task?.style?.imgURL && utilService.getExtension(task.style.imgURL) === 'video' &&
-            (<div className="task-details-cover img " >    
-               <video height="160" width= '100%' muted controls><source src={task.style.imgURL} type="video/mp4"></source></video>
+            (<div className="task-details-cover img" >
+               <video height="160" width='100%' muted controls><source src={task.style.imgURL} type="video/mp4"></source></video>
                <button ref={buttonRef} onClick={(ev) => { ev.stopPropagation(); onModal('Cover') }}><FaWindowMaximize />Cover</button>
             </div>
             )}
+
+         {isFullScreen &&
+            <div className="background-blur">
+               <button className="go-back-button" onClick={onGoBack}><VscClose className="close-icon" style={{color: '#ffffff'}} /></button>
+               <div className="full-screen">
+                  <div className="task-details-cover img" style={{ backgroundImage: `url('${task.style.imgURL}')`, height: '80vh' }}></div>
+               </div>
+            </div>}
 
       </header>
    )
